bf7dab0e429b450582614e3d09292439a7585697
Misaka-blog <misakablog@protonmail.com>
2024-04-14 15:03:06 +0800

    up

---
+++ test/acme.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora")
+PACKAGE_UPDATE=("apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "yum -y install")
+PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove" "yum -y remove")
+PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove")
+
+[[ $EUID -ne 0 ]] && red "注意：请在root用户下运行脚本" && exit 1
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
+        SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+    fi
+done
+
+[[ -z $SYSTEM ]] && red "不支持当前VPS系统, 请使用主流的操作系统" && exit 1
+
+check_ip(){
+    ipv4=$(curl -s4m8 ip.sb -k | sed -n 1p)
+    ipv6=$(curl -s6m8 ip.sb -k | sed -n 1p)
+}
+
+inst_acme(){
+    if [[ ! $SYSTEM == "CentOS" ]]; then
+        ${PACKAGE_UPDATE[int]}
+    fi
+    ${PACKAGE_INSTALL[int]} curl wget sudo socat openssl dnsutils
+
+    if [[ $SYSTEM == "CentOS" ]]; then
+        ${PACKAGE_INSTALL[int]} cronie
+        systemctl start crond
+        systemctl enable crond
+    else
+        ${PACKAGE_INSTALL[int]} cron
+        systemctl start cron
+        systemctl enable cron
+    fi
+
+    read -rp "请输入注册邮箱 (例: admin@gmail.com, 或留空自动生成一个gmail邮箱): " email
+    if [[ -z $email ]]; then
+        automail=$(date +%s%N | md5sum | cut -c 1-16)
+        email=$automail@gmail.com
+        yellow "已取消设置邮箱, 使用自动生成的gmail邮箱: $email"
+    fi
+
+    curl https://get.acme.sh | sh -s email=$email
+    source ~/.bashrc
+    bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+    
+    switch_provider
+
+    if [[ -n $(~/.acme.sh/acme.sh -v 2>/dev/null) ]]; then
+        green "Acme.sh 证书一键申请脚本安装成功!"
+    else
+        red "抱歉, Acme.sh 证书一键申请脚本安装失败"
+        green "建议如下："
+        yellow "1. 检查 VPS 的网络环境"
+        yellow "2. 脚本可能跟不上时代, 建议截图发布到 GitHub Issues 询问"
+    fi
+}
+
+unst_acme() {
+    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && yellow "未安装Acme.sh, 卸载程序无法执行!" && exit 1
+    ~/.acme.sh/acme.sh --uninstall
+    sed -i '/--cron/d' /etc/crontab >/dev/null 2>&1
+    rm -rf ~/.acme.sh
+    green "Acme.sh 证书一键申请脚本已彻底卸载!"
+}
+
+switch_provider(){
+    [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]] && inst_acme
+
+    yellow "请选择证书提供商, 默认通过 Letsencrypt.org 来申请证书 "
+    yellow "如果证书申请失败, 例如一天内通过 Letsencrypt.org 申请次数过多, 可选 BuyPass.com 或 ZeroSSL.com 来申请."
+    echo -e " ${GREEN}1.${PLAIN} Letsencrypt.org ${YELLOW}(默认)${PLAIN}"
+    echo -e " ${GREEN}2.${PLAIN} BuyPass.com"
+    echo -e " ${GREEN}3.${PLAIN} ZeroSSL.com"
+    read -rp "请选择证书提供商 [1-3]: " provider
+    case $provider in
+        2) bash ~/.acme.sh/acme.sh --set-default-ca --server buypass && green "切换证书提供商为 BuyPass.com 成功！" ;;
+        3) bash ~/.acme.sh/acme.sh --set-default-ca --server zerossl && green "切换证书提供商为 ZeroSSL.com 成功！" ;;
+        *) bash ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt && green "切换证书提供商为 Letsencrypt.org 成功！" ;;
+    esac
+}
+
+menu() {
+    clear
+    echo "#############################################################"
+    echo -e "#                   ${RED}Acme  证书一键申请脚本${PLAIN}                  #"
+    echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
+    echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
+    echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
+    echo -e "# ${GREEN}GitLab 项目${PLAIN}: https://gitlab.com/Misaka-blog               #"
+    echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakanocchannel              #"
+    echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/misakanoc                     #"
+    echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog        #"
+    echo "#############################################################"
+    echo ""
+    echo -e " ${GREEN}1.${PLAIN} 安装 Acme.sh 域名证书申请脚本"
+    echo -e " ${GREEN}2.${PLAIN} ${RED}卸载 Acme.sh 域名证书申请脚本${PLAIN}"
+    echo " -------------"
+    echo -e " ${GREEN}3.${PLAIN} 申请单域名证书 ${YELLOW}(80 端口申请)${PLAIN}"
+    echo -e " ${GREEN}4.${PLAIN} 申请单域名证书 ${YELLOW}(CF API申请)${PLAIN} ${GREEN}(无需解析)${PLAIN} ${RED}(不支持freenom域名)${PLAIN}"
+    echo -e " ${GREEN}5.${PLAIN} 申请泛域名证书 ${YELLOW}(CF API申请)${PLAIN} ${GREEN}(无需解析)${PLAIN} ${RED}(不支持freenom域名)${PLAIN}"
+    echo " -------------"
+    echo -e " ${GREEN}6.${PLAIN} 查看已申请的证书"
+    echo -e " ${GREEN}7.${PLAIN} 撤销并删除已申请的证书"
+    echo -e " ${GREEN}8.${PLAIN} 手动续期已申请的证书"
+    echo -e " ${GREEN}9.${PLAIN} 切换证书颁发机构"
+    echo " -------------"
+    echo -e " ${GREEN}0.${PLAIN} 退出脚本"
+    echo ""
+    read -rp "请输入选项 [0-9]: " menuInput
+    case "$menuInput" in
+        1 ) inst_acme ;;
+        2 ) unst_acme ;;
+        3 ) acme_standalone ;;
+        4 ) acme_cfapiTLD ;;
+        5 ) acme_cfapiNTLD ;;
+        6 ) view_cert ;;
+        7 ) revoke_cert ;;
+        8 ) renew_cert ;;
+        9 ) switch_provider ;;
+        * ) exit 1 ;;
+    esac
+}
+
+menu
